29fef49209c51bb9864e8ef6c207ca348857f91c
Abraham Medina Carrillo <abraham.medina.carrillo@uabc.edu.mx>
2021-06-20 15:00:07 -0700

    Practica 4 - Decodificador para Memoria (uso de compuertas logicas)

---
+++ Laboratorio/Practica 4/main.c
@@ -0,0 +1,204 @@
+/* =====================================================================
+* @author Abraham Medina Carrillo
+* @email abraham.medina.carrillo@uabc.edu.mx
+*
+* @website https://github.com/Medina1402
+* @repository ** private **
+* ===================================================================== */
+#include "stdio.c"
+
+#define DEFAULT_SEGMENT 0
+#define LINES_BUS_ADDR 11
+#define LINES_BUS_DATA 8
+
+
+/* =====================================================================*/
+extern unsigned char peek(unsigned int segment, unsigned int offset);
+extern void poke(unsigned int segment, unsigned int offset, unsigned char data);
+
+unsigned char TestDataBus(unsigned int segment, unsigned int offset);
+unsigned int TestAddrBus(unsigned int segment, unsigned int offset);
+
+unsigned char err_sRAM(unsigned char statusDataBus, unsigned int statusAddrBus, unsigned char showStatus);
+void __printTestDataBus(unsigned char status);
+void __printTestAddrBus(unsigned int status);
+
+void setTimeOut(unsigned int timer);
+void inc_B16(char * number);
+/* =====================================================================*/
+
+unsigned int memory_offset[2] = {0xA7FC, 0x27FC};  /*{0xA800, 0x2800}*/
+char memory_offset_str[2][7] = {"0xA7FC\0","0x27FC\0"};
+
+int main(void){
+    unsigned int test_addr;
+    unsigned char test_bus;
+    unsigned char index = 0, state_sRAM, counter;
+
+    puts(">>> Decodificadores\n\rPresione una tecla para continuar...\n\r");
+    getchar();
+    
+    for(counter = 0; counter < 61; counter++) {
+        test_bus = TestDataBus(DEFAULT_SEGMENT, memory_offset[index]);
+        test_addr = TestAddrBus(DEFAULT_SEGMENT, memory_offset[index]);
+        
+        puts((char *) memory_offset_str[index]);
+        puts(" - ");
+        state_sRAM = err_sRAM(test_bus, test_addr, 0);
+        
+        if (state_sRAM == 2) puts(" ERR        ");
+        else if (state_sRAM == 0) puts(" OK         ");
+        
+        if (index == 1) if (state_sRAM%2 == 0) puts("\n\r");
+        
+        inc_B16(memory_offset_str[index]);
+        memory_offset[index]++;
+
+        index = counter%2;
+        setTimeOut(6000);
+        
+        if(counter%20 == 1 && counter > 1) {
+            puts("-- \n");
+            getchar();
+        }
+    }
+
+    puts("\n ================================= \n\n\n");
+    getchar();
+    return 0;
+}
+
+/*
+*
+*/
+unsigned char TestDataBus(unsigned int segment, unsigned int offset) {
+    unsigned char pattern = 0;
+    unsigned char value;
+
+    for(value = 1; value; value <<= 1) {
+        poke(segment, offset, value); /* Escribimos 1 */
+        /* Comparamos el contenido con lo que escribimos */
+        if( peek(segment, offset) != value ) pattern |= value;
+    }
+
+    return pattern;
+}
+
+/*
+*
+*/
+unsigned int TestAddrBus(unsigned int segment, unsigned int offset) {
+    unsigned char match_pattern = 0xAA;
+    unsigned int ram_disp = 1;
+    unsigned int pattern = 0;
+
+    while(ram_disp < (1 << LINES_BUS_ADDR)) {
+        poke(segment, offset + ram_disp, pattern);
+        ram_disp <<= 1;
+    }
+
+    poke(segment, offset, match_pattern);
+
+    for(ram_disp = 1; ram_disp < (1 << LINES_BUS_ADDR); ram_disp <<= 1) {
+        if( peek(segment, offset + ram_disp) == match_pattern || peek(segment, offset + ram_disp) != 0) {
+            pattern |= ram_disp;
+        }
+    }
+
+    return pattern;
+}
+
+/* ================= PRINT TEST ================= */
+/**
+ * Busca los bits activos en el "status" e indica el estado (0 = ok, 1 = err)
+ * @param status Bits a recorrer (numero hexadecimal <representacion binaria cruda>)
+ */
+void __printTestDataBus(unsigned char status) {
+    unsigned char _count = 0;
+    puts("*** TEST DATA BUS ***\n\r");
+
+    while(_count<8) {
+        puts(" > Line ");
+        putchar(_count+'0');
+        puts(": ");
+
+        if(status & (1 << _count)) puts(" - ERR\n\r");
+        else puts(" - OK\n\r");
+
+        _count++;
+    }
+    puts("\n\r");
+}
+
+/**
+ *
+ * @param status
+ * @param linesAddr
+ */
+void __printTestAddrBus(unsigned int status) {
+    unsigned int _count = 0;
+    puts("*** TEST ADDR BUS ***\n\r");
+
+    while(_count<11) {
+        puts(" > Line ");
+        putchar(_count>9 ? _count+7+'0' :_count+'0');
+        puts(": ");
+
+        if(status & (1 << _count)) puts(" - ERR\n\r");
+        else puts(" - OK\n\r");
+
+        _count++;
+    }
+    puts("\n\r");
+}
+/**
+ *
+ * @param statusDataBus Estado del bus de datos (usar el retorno de testAddrBus)
+ * @param statusAddrBus Estado del bus de direcciones (usar el retorno de testAddrBus)
+ * @param showStatus Mostrar estado en caso de no ser un error catastrofico
+ * @return
+ */
+unsigned char err_sRAM(unsigned char statusDataBus, unsigned int statusAddrBus, unsigned char showStatus) {
+    if(showStatus) {
+        __printTestAddrBus(statusAddrBus);
+        __printTestDataBus(statusDataBus);
+    }
+
+    if(statusDataBus >= 0xFF && statusAddrBus >= 0xFF) {
+        puts("\n\r=== \n\r||| *** FALLO CATASTROFICO *** ||| \n\r===\n\r");
+        return 1;
+    } else if(statusAddrBus != 0 || statusDataBus != 0) return 2;
+
+    return 0;
+}
+
+
+/* ================= PRINT ================= */
+/**
+ * Realiza un conteo para simular una pausa 
+*/
+void setTimeOut(unsigned int timer) {
+    while(--timer > 0);
+}
+
+
+/**
+ * Incrementa los ultimos 3 digitos de un numero Hexadecimal
+ * 0 -> F
+ * 
+*/
+void inc_B16(char * number) {
+    number[5]++;
+
+    if(number[5]-1 == '9') number[5]+=7;
+    else if(number[5]-1 == 'f' || number[5]-1 == 'F') {
+        number[4]++;
+        number[5] = '0';
+
+        if(number[4]-1 == '9') number[4]+=7;
+        else if(number[4]-1 == 'f' || number[4]-1 == 'F') {
+            number[3]++;
+            number[4] = '0';
+        }
+    }
+}
